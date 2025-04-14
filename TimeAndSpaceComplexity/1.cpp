fc12b5ac68069cf61b8c10150da8d2ad73d25afb
aksh31at <asharma21_be22@thapar.edu>
2025-04-14 14:27:59 +0530

    Added files

---
+++ TimeAndSpaceComplexity/1.cpp
@@ -0,0 +1,8 @@
+//Time Complexity: It is defined as the time taken by the algorithm to run as a function of the length of the input.
+
+//suppose for a website people who visit(users) act as the input.
+//we choose best case, average case and worst case to analyze the time complexity.
+
+//we consider worst case more often than best case and average case because it is more useful in real life.
+//Asymptotic Notation: Big O Notation, Omega Notation, Theta Notation
+
